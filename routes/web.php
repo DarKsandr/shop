34b7b7565bed57b69ab2fe6a2041eeddb7ae0c76
DarKsandr <gdarksandr@gmail.com>
2022-09-01 14:51:00 +0400

    Add view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,49 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name("index");
+
+Route::get('/about', function () {
+    return view('about');
+})->name("about");
+
+Route::get('/login', function () {
+    return view('login');
+})->name("login");
+
+Route::get('/my-account', function () {
+    return view('my-account');
+})->name("my-account");
+
+Route::get('/faq', function () {
+    return view('faq');
+})->name("faq");
+
+Route::get('/coming-soon', function () {
+    return view('coming-soon');
+})->name("coming-soon");
+
+Route::get('/compare', function () {
+    return view('compare');
+})->name("compare");
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name("contact");
+
+Route::get('/cart', function () {
+    return view('cart.cart');
+})->name("cart");
+
+Route::get('/wishlist', function () {
+    return view('wishlist');
+})->name("wishlist");
+
+Route::get('/thank-you-page', function () {
+    return view('thank-you-page');
+})->name("thank-you-page");
+
+Route::get('/order-tracking', function () {
+    return view('order-tracking');
+})->name("order-tracking");
